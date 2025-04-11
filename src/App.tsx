164a6a259e7009ec67f7df1e8787047bee380d20
Sashen Pasindu <sashenpasindu@gmail.com>
2025-04-11 10:26:50 +0200

    feat: add buyme coffee link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useDefaultCountry } from "./hooks/useDefaultCountry";
 import Content from "@/Content";
+import buymecoffee from "@/assets/buymecoffee_logo.svg";
 
 function App() {
   const { country, userTimezone } = useDefaultCountry();
@@ -11,7 +12,18 @@ function App() {
       )}
       {/* {!country && <Content userCountry={"LK"} userTimezone={"Asia/Colombo"} />} */}
       <footer className="mt-auto py-4 text-center text-sm border-t border-gray-200">
-        Made with ❤️ by <a href="https://sashen.me">Sashen</a>
+        Made with ❤️ by <a href="https://sashen.me">@SashenMe</a>
+        <a
+          className="pt-2 block"
+          target="_blank"
+          href="https://buymeacoffee.com/sashenme"
+        >
+          <img
+            className="mx-auto max-w-[120px]"
+            src={buymecoffee}
+            alt="Buy me a cofee!"
+          />
+        </a>
       </footer>
     </div>
   );
